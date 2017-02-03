f330a71688cd5c650786592836d23eaa7c8a8212
Lucas Feijó Portela <lucasfjportela@gmail.com>
2017-02-03 18:36:22 -0300

    Desfazendo alterações que deram merda

--- cena.cpp
+++ cena.cpp
@@ -84,22 +84,6 @@ return (w == 0) ? h : mdc(w, h%w);
 }
 
 void Cena::printIMG(vec origin, int w, int h, vector <Light> lights){
-	String log = "Renderizando.";
-	char dot = '.';
-	int counter = 1;
-	
-	if(counter == 3){
-		println("Renderizando.");
-		counter = 1;
-	} else if(counter == 1){
-		print(dot);
-		counter++;
-	} else if(counter == 2){
-		print(dot);
-		counter++;
-	}
-
-
 	FILE *arq = NULL;
 
 	int p;
