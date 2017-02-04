6c024a63c2e25952cc729d3f40b4213f9306bfd0
dayvidcds <dayvid.clementino@gmail.com>
2017-02-04 10:43:19 -0300

    inclusão do método que faz a leitura de arquivos .mtl

--- cena.cpp
+++ cena.cpp
@@ -1,11 +1,11 @@
 #include "cena.hpp"
 
 
-void Cena::addObjects(Object *obj){
+void Cena::addObjects(Object *obj) {
 	this->objects.push_back(obj);
 }
 
-void Cena ::readObjects(FILE *arqObjects) {
+void Cena :: readObjects(FILE *arqObjects) {
 
 	char linha[256], tipo[16], nomeMaterial[32];;
 	int qtdVertices = 0;
@@ -16,12 +16,11 @@ void Cena ::readObjects(FILE *arqObjects) {
 	int quant_obj = -1;
 	
 	vector <Object *> objects;
-	//vector <Material*> materiais;
 	vec vertice(3);
 	vec norm(3);
 	vec dColor;
 	vec eColor;
-
+	vector <vec> colorFace;
 	dColor << 51.0 << 51.0 << 51.0;
 	eColor << 51.0 << 51.0 << 51.0;
 
@@ -35,22 +34,15 @@ void Cena ::readObjects(FILE *arqObjects) {
 			++quant_obj;
 			objt = new Object();
 			addObjects(objt);
-			//cout << "Objeto" << endl;
-			//objects.push_back(objeto);
+			cout << "obj" << endl;
 		}
 		else if (!strcmp(tipo, "v")) {
 			sscanf(linha, "%*s %lf %lf %lf", &vertice[0], &vertice[1], &vertice[2]);
-			//cout << "alow" << endl;
 			objt->addVertices(vertice);
-			//qtdVertices++;
-			//cout << vertice[0] << "  " << vertice[1] << "  " << vertice[2] << endl;
-			//return objects;
 		}
 		else if (!strcmp(tipo, "vn")) {
 			sscanf(linha, "%*s %lf %lf %lf", &norm[0], &norm[1], &norm[2]);
-			//cout << "Entrei" << endl;
 			objt->addNormais(norm);
-			//qtdVetoresNormais++;
 		}
 		else if (!strcmp(tipo, "f")) {
 			sscanf(linha, "%*s %d//%d %d//%d %d//%d", &vert1, &norm1, &vert2, &norm2, &vert3, &norm3);
@@ -65,17 +57,33 @@ void Cena ::readObjects(FILE *arqObjects) {
 			objt->addTriang(triang);
 		}
 		else if (strcmp(tipo, "mtllib") == 0) {
-
-			char nomeArquivoMateriais[32];
-
-			sscanf(linha, "%s %s", tipo,  nomeArquivoMateriais);
-			FILE *arquivoMateriais = fopen(nomeArquivoMateriais, "r");
-
-			//materiais = lerMateriais(arquivoMateriais);
+			//char nomeArquivoMateriais[32];
+			//sscanf(linha, "%s %s", tipo,  nomeArquivoMateriais);
+			//FILE *arquivoMateriais = fopen(nomeArquivoMateriais, "r");
 		}
 		else if (strcmp(tipo, "usemtl") == 0) {
 			sscanf(linha, "%s %s", tipo, nomeMaterial);
 		}
+	}
+}
+
+vector <vec> Cena :: readMaterials(FILE *arquivoMateriais) {
+	char linha[256], tipo[16], nomeMaterial[32];
+	vec cor(3);
+	vector <vec> colorCoords;
+
+	while (fgets(linha, sizeof(linha), arquivoMateriais)) {
+
+		sscanf(linha, "%s ", tipo);
+
+		if (!strcmp(tipo, "newmtl")) {
+			sscanf(linha, "%s %s", tipo, nomeMaterial);
+		}
+		else if (!strcmp(tipo, "Kd")) {
+			sscanf(linha, "%*s %lf %lf %lf", &cor[0], &cor[1], &cor[2]);
+			colorCoords.push_back(cor);
+		}
+		return colorCoords;
 	}
 }
 
@@ -85,7 +93,7 @@ return (w == 0) ? h : mdc(w, h%w);
 
 void Cena::printIMG(vec origin, int w, int h, vector <Light> lights){
 	FILE *arq = NULL;
-
+	cout << "renderizando..." << endl;
 	int p;
 
 	int quantObjects = this->objects.size();
@@ -166,31 +174,3 @@ void Cena::printIMG(vec origin, int w, int h, vector <Light> lights){
 		}
 	}
 }
-/*
-vector <Material*> lerMateriais(FILE *arquivoMateriais) {
-	char linha[256], tipo[16], nomeMaterial[32];
-	vec cor(3);
-
-	vector <Material*> mate;
-	Material *material;
-
-	while (fgets(linha, sizeof(linha), arquivoMateriais)) {
-
-		sscanf(linha, "%s ", tipo);
-
-		if (strcmp(tipo, "newmtl") == 0) {
-			
-			sscanf(linha, "%s %s", tipo, nomeMaterial);
-			material = new Material();
-			strcpy(material->nome_,nomeMaterial);
-		
-		}
-		else if (strcmp(tipo, "Kd") == 0) {
-			sscanf(linha, "%*s %lf %lf %lf", &cor[0], &cor[1], &cor[2]);
-			material->difuseColor << cor[0] << cor[1] << cor[2];
-			mate.push_back(material);
-		}
-			return mate;
-
-	}
-	*/
